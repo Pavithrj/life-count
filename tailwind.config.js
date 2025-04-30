96bc4a883a97c945a3ac7f9fe86a63347580794f
Pavithr Jain <pavithrjain19@gmail.com>
2025-04-30 06:40:16 +0530

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import("tailwindcss").Config} */
 
 export default {
+    darkMode: 'class',
     content: [
         "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}"
+        "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {}
+        extend: {
+            animation: {
+                fadeInDown: 'fadeInDown 0.6s ease-out forwards',
+            },
+            keyframes: {
+                fadeInDown: {
+                    '0%': {
+                        opacity: 0,
+                        transform: 'translateY(-20px)',
+                    },
+                    '100%': {
+                        opacity: 1,
+                        transform: 'translateY(0)',
+                    },
+                },
+            },
+        },
     },
-    plugins: []
+    plugins: [],
 };
